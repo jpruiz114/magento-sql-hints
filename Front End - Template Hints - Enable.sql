1e353f00c868393918348bde6f71f5f55a2276e5
Jean Paul Ruiz Vallejo <jeanpaul-vp@pet360.com>
2015-02-11 16:30:32 -0500

    Simple SQL change.

--- Front End - Template Hints - Enable.sql
+++ Front End - Template Hints - Enable.sql
@@ -2,4 +2,4 @@ SET SQL_SAFE_UPDATES = 0;
 
 UPDATE core_config_data ccd
 SET ccd.value = 1
-WHERE path IN ('dev/debug/template_hints','dev/debug/template_hints_blocks');
+WHERE path IN ('dev/debug/template_hints', 'dev/debug/template_hints_blocks');
